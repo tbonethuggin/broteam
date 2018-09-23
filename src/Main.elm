ae2b2fbf8371cd9add816b74606e75dd5fcb7c5b
Kevin <bca.kevlu@gmail.com>
2018-09-23 10:38:02 -0400

    get current date

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,5 @@
 import Time
+import Task
 import Browser
 
 import Html exposing (Html, button, div, text)
@@ -13,8 +14,10 @@ main =
     }
 
 type alias Model =
-  { workoutCurrent : Workout
+  { workoutCurrent : Maybe Workout
   , workoutHistory : List Workout
+  , timeZone : Time.Zone
+  , currentDate : Maybe Date
   }
 
 type alias Date =
@@ -22,6 +25,13 @@ type alias Date =
   , month : Int
   , day : Int
   }
+
+posixToDate : Time.Zone -> Time.Posix -> Date
+posixToDate zone posix =
+  Date
+    (Time.toYear zone posix)
+    (monthToInt (Time.toMonth zone posix))
+    (Time.toDay zone posix)
 
 dateToString : Date -> String
 dateToString date =
@@ -38,23 +48,51 @@ type alias Workout =
   , exercises : List Exercise
   }
 
-newWorkout : Workout
-newWorkout =
-  Workout (Date 2018 4 1) []
+newWorkout : Date -> Workout
+newWorkout date =
+  Workout date []
+
+
+monthToInt : Time.Month -> Int
+monthToInt month =
+  case month of
+    Time.Jan -> 1
+    Time.Feb -> 2
+    Time.Mar -> 3
+    Time.Apr -> 4
+    Time.May -> 5
+    Time.Jun -> 6
+    Time.Jul -> 7
+    Time.Aug -> 8
+    Time.Sep -> 9
+    Time.Oct -> 10
+    Time.Nov -> 11
+    Time.Dec -> 12
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model newWorkout []
-  , Cmd.none
+  ( Model Maybe.Nothing [] Time.utc Maybe.Nothing
+  , Task.perform SetTimeZone Time.here
   )
 
 type Msg =
-  Increment
-  | Decrement
+  SetTimeZone Time.Zone
+  | SetTime Time.Posix
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    (model, Cmd.none)
+  case msg of
+    SetTimeZone newTimeZone ->
+      ( { model | timeZone = newTimeZone }
+      , Task.perform SetTime Time.now
+      )
+    SetTime currentTime ->
+      let
+        date = posixToDate model.timeZone currentTime
+      in
+        ( { model | currentDate = Maybe.Just date, workoutCurrent = Maybe.Just (newWorkout date) }
+        , Cmd.none
+        )
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -62,6 +100,13 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ div [] [ text (dateToString model.workoutCurrent.date) ]
-    ]
+  let
+    dateStr = case model.currentDate of
+      Nothing ->
+        ""
+      Just date ->
+        dateToString date
+  in
+    div []
+      [ div [] [ text dateStr ]
+      ]
